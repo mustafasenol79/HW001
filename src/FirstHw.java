f6a550338363f9f7bc32117beffa6bb3ad513517
mustafasenol79 <msmno79@gmail.com>
2022-09-26 23:25:00 +0300

    hw1bıttı

--- src/FirstHw.java
+++ src/FirstHw.java
@@ -1,5 +1,7 @@
 public class FirstHw {
     public static void main(String[] args) {
         //TODO: Lütfen ekrana Merhaba Dünya! yazan bir java programı yazınız.
+        System.out.println(" merhaba Dünya ");
+
     }
 }
